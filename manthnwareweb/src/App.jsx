00d253f6aa54992cf17d487a3bf0af59d781c811
vijay04111 <vijaykhot2170@gmail.com>
2026-07-13 11:46:47 +0530

    updated Footer section

--- manthnwareweb/src/App.jsx
+++ manthnwareweb/src/App.jsx
@@ -25,6 +25,7 @@ function Home() {
       <Features />
       <DiscoverMore />
       <Contact />
+      
     </>
   );
 }
--- manthnwareweb/src/components/layout/Footer.jsx
+++ manthnwareweb/src/components/layout/Footer.jsx
@@ -1,36 +1,169 @@
-import logo from "../../assets/images/logo.png";
+import React from "react";
+import "../home/Home CSS/Footer.css";
+
+const quickLinks = [
+  { name: "Home", link: "/" },
+  { name: "About Us", link: "/about" },
+  { name: "Events", link: "/events" },
+  { name: "Gallery", link: "/gallery" },
+  { name: "Contact", link: "/contact" },
+];
+
+const programs = [
+  "State Level GK Exam",
+  "Scholarship Program",
+  "Social Activities",
+  "Career Guidance",
+  "Women Empowerment",
+];
+
+const socialLinks = [
+  {
+    icon: "bi bi-facebook",
+    link: "#",
+  },
+  {
+    icon: "bi bi-instagram",
+    link: "#",
+  },
+  {
+    icon: "bi bi-youtube",
+    link: "#",
+  },
+  {
+    icon: "bi bi-linkedin",
+    link: "#",
+  },
+];
 
 const Footer = () => {
   return (
-    <footer className="bg-[#b5b4af] py-10">
+    <>
+      <footer className="footer-section">
 
-      <div className="mx-auto flex max-w-[1450px] items-center justify-between px-10">
+        <div className="container">
 
-        {/* Logo */}
+          <div className="row gy-5">
 
-        <img
-          src={logo}
-          alt="Logo"
-          className="w-[95px]"
-        />
+            {/* About */}
 
-        {/* Copyright */}
+            <div className="col-lg-4 col-md-6">
 
-        <div className="text-center">
+              <h3 className="footer-logo">
+                Sankalpa Welfare Foundation
+              </h3>
 
-          <p className="text-[20px] text-[#222]">
-            © 2026 Manthan Welfare Foundation.
-          </p>
+              <p className="footer-about">
+               Sankalpa Welfare Foundation is committed to empowering society
+                through education, scholarships, social awareness, and
+                community development initiatives.
+              </p>
 
-          <p className="mt-2 text-[20px] text-[#222]">
-            All Rights Reserved.
-          </p>
+              <div className="social-icons">
+
+                {socialLinks.map((item, index) => (
+                  <a
+                    key={index}
+                    href={item.link}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <i className={item.icon}></i>
+                  </a>
+                ))}
+
+              </div>
+
+            </div>
+
+            {/* Quick Links */}
+
+            <div className="col-lg-2 col-md-6">
+
+              <h5>Quick Links</h5>
+
+              <ul>
+
+                {quickLinks.map((item, index) => (
+
+                  <li key={index}>
+                    <a href={item.link}>{item.name}</a>
+                  </li>
+
+                ))}
+
+              </ul>
+
+            </div>
+
+            {/* Programs */}
+
+            <div className="col-lg-3 col-md-6">
+
+              <h5>Programs</h5>
+
+              <ul>
+
+                {programs.map((item, index) => (
+
+                  <li key={index}>
+                    <a href="/">{item}</a>
+                  </li>
+
+                ))}
+
+              </ul>
+
+            </div>
+
+            {/* Contact */}
+
+            <div className="col-lg-3 col-md-6">
+
+              <h5>Contact Us</h5>
+
+              <p>
+                <i className="bi bi-geo-alt-fill"></i>
+
+                B5, Karan Court,
+                Ahilyanagar,
+                Maharashtra - 414001
+
+              </p>
+
+              <p>
+                <i className="bi bi-telephone-fill"></i>
+
+                +91 9130093832
+
+              </p>
+
+              <p>
+                <i className="bi bi-envelope-fill"></i>
+
+                info@Sankalpawelfarefoundation.org
+
+              </p>
+
+            </div>
+
+          </div>
+
+          <hr />
+
+          <div className="footer-bottom">
+
+            <p>
+              © {new Date().getFullYear()} Sankalpa Welfare Foundation. All
+              Rights Reserved.
+            </p>
+
+          </div>
 
         </div>
 
-      </div>
-
-    </footer>
+      </footer>
+    </>
   );
 };
 
